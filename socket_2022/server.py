23400b97f896e080b5d94820b8c7fa3c99fca35f
999Kit <kietnguyentuan911@gmail.com>
2022-12-21 12:15:42 +0700

    kiet

--- socket_2022/server.py
+++ socket_2022/server.py
@@ -1,7 +1,6 @@
 import socket
-import threading
+import _thread
 import server_config
-
 
 content_type_dict = {
     "html": "text/html",
@@ -15,107 +14,86 @@ content_type_dict = {
     "ico": "image/x-icon"
 }
 
+def get_filename_content_type(headers):
+    filename = headers[0].split()[1]
+    if filename == '/':
+        filename = '/index.html'
 
-def read_request(client_socket):
-    request = ""  # set this for time out case
-    client_socket.settimeout(1)  # block socket operations for 5 sec
-
-    try:
-        request = client_socket.recv(server_config.BUFFERSIZE).decode()
-    except TimeoutError:
-        if not request:
-            print("DIDN'T RECEIVE DATA. [TIMEOUT]")
-    finally:
-        return request
-
-
-def handle_client(client_socket):
-    while True:
-        # request = read_request(client_socket)
-        client_socket.settimeout(2)
-        try:
-            request = client_socket.recv(server_config.BUFFERSIZE).decode()
-        except socket.timeout:
-            print("DIDN'T RECEIVE DATA. [TIMEOUT]")
-            break
-
-        if not request:
-            # header = """HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"""
-            header = """HTTP/1.1 200 OK\r\nConnection: close"""
-            client_socket.send(header.encode())
-            break
-
-        if "POST" in request:
-            if not check_login(request, client_socket):
-                # client_socket.close()
-                break
-        print(f"[HTTP REQUEST]\n{request}")
-
-        headers = request.split('\n')
-        filename = headers[0].split()[1]
-        if filename == '/':
-            filename = 'index.html'
-
-        if '.' in filename:
-            file_type = filename.split('.')[1]
-        else:
-            file_type = ""
-        # Return 404 when file does not exist
-        try:
-            with open('.' + filename, 'rb') as f:
-                content = f.read()
-        except FileNotFoundError:
-            response = "HTTP/1.1 404 NOT FOUND\r\nConnection: close\r\n\r\nFile Not Found"
-            client_socket.sendall(response.encode())
-            break
-
-        if file_type not in content_type_dict:
-            content_type = "application/octet-stream"
-        else:
-            content_type = content_type_dict[file_type]
-
-        # Send HTTP response
-        header = """HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n""" % (content_type, len(content))
-
-        client_socket.sendall(header.encode() + content)
-        # client_socket.send(content)
-
-    client_socket.close()
-
-
-def accept_incoming_connections(server_socket):
-    """Sets up handling for incoming clients."""
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"[NEW CONNECTION] {client_address} connected.")
-
-        request_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        request_thread.daemon = True
-        request_thread.start()
-        request_thread.join()
-
-        print(f"[NEW CONNECTION] {client_address} closed.")
-
-    # handle_client(client_socket)
+    if '.' in filename:
+        file_type = filename.split('.')[1]
+    else:
+        file_type = ""
+    if file_type not in content_type_dict:
+        content_type = "application/octet-stream"
+    else:
+        content_type = content_type_dict[file_type]
+    return filename, content_type
 
 
 def check_login(request, client_socket):
     if "uname=admin&psw=123456" not in request:
-        response = """HTTP/1.1 401 Unauthorized\r\nConnection: close\r\n\r\n<!DOCTYPE html>
-        <h1>401 Unauthorized</h1><p>This is a private area.</p>"""
+        response = "HTTP/1.1 401 Unauthorized\r\nConnection: close\r\n\r\n" \
+                   "<!DOCTYPE html><h1>401 Unauthorized</h1><p>This is a private area.</p>"
         client_socket.send(response.encode())
         return False
     return True
 
+def handle_client(client_socket, client_address):
+    print(f"\n[NEW CONNECTION] {client_address} connected.")
+
+    while True:
+        request = ""
+        client_socket.settimeout(5)
+        try:
+            request = client_socket.recv(server_config.BUFFERSIZE).decode()
+        except socket.timeout:
+            print("\n[REQUEST TIMEOUT]")
+            header = "HTTP/1.1 408 Request Time-out\r\nConnection: close\r\n\r\n"
+            client_socket.send(header.encode())
+            break
+
+        if not request:
+            break
+
+        if "POST" in request:
+            if not check_login(request, client_socket):
+                break
+
+        headers = request.split('\n')
+        # print(f"[HTTP REQUEST]\n{headers[0]}")
+        print(f"{client_address} {headers[0]}")
+
+        filename, content_type = get_filename_content_type(headers)
+        # Return 404 when file does not exist
+
+        try:
+            with open("." + filename, 'rb') as f:
+                content = f.read()
+            header = """HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n""" % \
+                     (content_type, len(content))
+            client_socket.sendall(header.encode() + content)
+        except FileNotFoundError:
+            response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\nFile Not Found"
+            client_socket.sendall(response.encode())
+            break
+
+    client_socket.close()
+    print(f"\n{client_address} closed.")
+
+
+def accept_incoming_connections(server_socket):
+    """Sets up handling for incoming clients."""
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        _thread.start_new_thread(handle_client, (client_socket, client_address))
+
+def start(server_socket):
+    print(f"[LISTENING] {socket.gethostbyname(socket.gethostname()), server_config.PORT} is listening")
+    server_socket.listen(50)
+    accept_incoming_connections(server_socket)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(server_config.ADDR)
-
-
-def start(server_socket):
-    print(f"[LISTENING] Server is listening")
-    server_socket.listen(5)
-    accept_incoming_connections(server_socket)
-
 
 start(server_socket)
